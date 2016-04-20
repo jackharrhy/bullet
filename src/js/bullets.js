65261f9d2f284c772a0ca71791c5a498261ea7dc
Jack Arthur Harrhy <jack@jackharrhy.com>
2016-04-19 22:37:22 -0230

    alpha 0.0.4 - optimization of the bullets

--- src/js/bullets.js
+++ src/js/bullets.js
@@ -1,67 +1,15 @@
-var menuCanvas = document.getElementById('menuCanvas'); var m = menuCanvas.getContext('2d');
+var menuCanvas = document.getElementById('menuCanvas'); var m = menuCanvas.getContext('2d'); menuCanvas.width = 512;
+menuCanvas.height = 32;
+var m = menuCanvas.getContext('2d');
 
 var gameCanvas = document.getElementById('gameCanvas');
-var g = gameCanvas.getContext('2d');
-
 gameCanvas.width = 512;
 gameCanvas.height = 512;
+var g = gameCanvas.getContext('2d');
 
-menuCanvas.width = 512;
-menuCanvas.height = 32;
-
-var player = {
-	type: 'player',
-	pos: {
-		x: gameCanvas.width/2,
-		y: gameCanvas.height/2
-	},
-	radius: 5,
-	speed: 3,
-	deinc: 0.1,
-	fillColor: '#333'
-};
-
-function fixBulletPos(bullet) {
-	if(bullet.pos.y < gameCanvas.height * -0.1) bullet.pos.y += gameCanvas.height * 1.2;
-	if(bullet.pos.y > gameCanvas.height * 1.1) bullet.pos.y -= gameCanvas.height * 1.2;
-
-	if(bullet.pos.x > gameCanvas.width * 1.1) bullet.pos.x -= gameCanvas.height * 1.2;
-	if(bullet.pos.x < gameCanvas.width * -0.1) bullet.pos.x += gameCanvas.height * 1.2;
-
-	if(bullet.type == 'player') {
-		for(var i=0, len = bullets.array.length; i < len; i++) {
-			var connectedBullet = bullets.array[i];
-			if(connectedBullet.isConnected) {
-				if(connectedBullet.pos.y > gameCanvas.height * 1.1) connectedBullet.pos.y -= gameCanvas.height * 1.2;
-
-				if(connectedBullet.pos.x > gameCanvas.width * 1.1) connectedBullet.pos.x -= gameCanvas.height * 1.2;
-				if(connectedBullet.pos.x < gameCanvas.width * -0.1) connectedBullet.pos.x += gameCanvas.height * 1.2;
-			}
-		}
-	}
+function vec2(x,y) {
+	return { x: x, y: y };
 }
-
-var pattern = {
-	down: function(bullet) {
-		bullet.pos.y += bullet.yspeed;
-		fixBulletPos(bullet);
-	},
-	sideWays: function(bullet) {
-		bullet.pos.y += bullet.yspeed;
-		bullet.pos.x += bullet.xspeed;
-		fixBulletPos(bullet);
-	},
-	wavyDown: function(bullet, key) {
-		bullet.pos.y += bullet.yspeed + Math.sin((frame + key)/30);
-		bullet.pos.x += Math.sin(frame/(bullet.xspeed * 40)) * 2;
-		fixBulletPos(bullet);
-	},
-	awayFromCenter: function(bullet, key) {
-
-	}
-};
-
-var bullets = { array: [] };
 
 function randColor() {
 	return '#' + Math.floor(Math.random()*16777215).toString(16);
@@ -71,20 +19,40 @@ function randInt(min, max) {
 	return Math.round(Math.random() * max) - min;
 }
 
-function generateBullet(i) {
-	var bullet = {
-		type: 'bullet',
-		pos: {
-			x: i,
-			y: 0
-		},
-		radius: 6 + (Math.random() * 4),
-		fillColor: randColor(),
-		yspeed: 1 + (2 * Math.random()),
-		xspeed: 1 + (2 * Math.random()),
-		pattern: pattern.sideWays
-	};
-	bullets.array.push(bullet);
+var player = {
+	type: 'player',
+	pos: vec2(gameCanvas.width/2, gameCanvas.height/2),
+	radius: 5,
+	speed: 3,
+	deinc: 0.1,
+	color: '#333'
+};
+
+var pattern = {
+	down: function(bullet) {
+		bullet.pos.y += bullet.speedpos.y;
+	},
+	sideWays: function(bullet) {
+		bullet.pos.y += bullet.speedpos.y;
+		bullet.pos.x += bullet.speedpos.x;
+	},
+	wavyDown: function(bullet) {
+		bullet.pos.y += bullet.speedpos.y + Math.sin((frame)/30);
+		bullet.pos.x += Math.sin(frame/(bullet.speedpos.x * 40)) * 2;
+	},
+	awayFromCenter: function(bullet, key) {
+
+	}
+};
+
+var bullets = { array: [] };
+
+function Bullet(pos,radius,color,speedpos,pattern) {
+	this.pos = pos;
+	this.radius = radius;
+	this.color = color;
+	this.speedpos = speedpos;
+	this.pattern = pattern;
 }
 
 var func = {
@@ -104,14 +72,11 @@ var func = {
 	},
 	distanceCheck: function(obj1, obj2) {
 		pytha = func.pytha(obj1.pos, obj2.pos);
-		if(pytha < 110) {
-			draw.line(g, obj1.pos, obj2.pos, pytha/18.33333, 'rgba(0,0,0,'+(((pytha * -1) + 110)/330).toString()+')');
-		}
 		return pytha < (obj1.radius + obj2.radius);
 	},
+
 	unclog: function(obj1, obj2) {
 		var angle = Math.acos((obj1.pos.x - obj2.pos.x)/func.pytha(obj1.pos, obj2.pos));
-
 		obj2.pos.x = obj1.pos.x - (Math.cos(angle) * (obj1.radius + obj2.radius));
 
 		var yMov = Math.sin(angle) * (obj1.radius + obj2.radius);
@@ -120,16 +85,25 @@ var func = {
 		} else {
 			obj2.pos.y = obj1.pos.y + (Math.sin(angle) * (obj1.radius + obj2.radius));
 		}
+	},
+
+	squareCheck: function(obj1, obj2) {
+		return !(
+			(obj1.pos.x + obj1.radius) < (obj2.pos.x - obj2.radius) ||
+			(obj1.pos.y + obj1.radius) < (obj2.pos.y - obj2.radius) ||
+			(obj1.pos.x - obj1.radius) > (obj2.pos.x + obj2.radius) ||
+			(obj1.pos.y - obj1.radius) > (obj2.pos.y + obj2.radius)
+		);
 	}
 };
 
 var update = {
-	collisions: function(bullet, i) {
+	collisions: function(bullet) {
 		if(bullet.isConnected) {
 			for(var b=0, len = bullets.array.length; b < len; b++) {
 				var otherBullet = bullets.array[b];
 				if(!otherBullet.isConnected &&
-					 func.distanceCheck(bullet, otherBullet)) {
+					 func.squareCheck(bullet, otherBullet) && func.distanceCheck(bullet, otherBullet)) {
 
 					func.unclog(bullet, otherBullet);
 					otherBullet.isConnected = true;
@@ -137,24 +111,36 @@ var update = {
 				}
 			}
 		} else {
-			if(func.distanceCheck(player, bullet)) {
+			if(func.squareCheck(player, bullet) && func.distanceCheck(player, bullet)) {
 				func.unclog(player, bullet);
 
 				bullet.isConnected = true;
 				player.speed -= bullet.radius / 60;
 			} else {
-				bullet.pattern(bullet, i);
+				bullet.pattern(bullet);
+				update.swapArround(bullet);
 			}
 		}
+	},
+
+	perfectCollision: function(bullet, i) {
+
+	},
+
+	swapArround: function(obj) {
+		if(obj.pos.y < gameCanvas.height * -0.1) obj.pos.y += gameCanvas.height * 1.2;
+		if(obj.pos.y > gameCanvas.height * 1.1)  obj.pos.y -= gameCanvas.height * 1.2;
+
+		if(obj.pos.x > gameCanvas.width * 1.1)   obj.pos.x -= gameCanvas.height * 1.2;
+		if(obj.pos.x < gameCanvas.width * -0.1)  obj.pos.x += gameCanvas.height * 1.2;
 	}
 };
 
 var draw = {
-	circle: function(ctx,obj) {
-		ctx.fillStyle = obj.fillColor;
-		ctx.lineWidth = 3;
+	circle: function(ctx,pos,radius,color) {
+		ctx.fillStyle = color;
 		ctx.beginPath();
-		ctx.arc(obj.pos.x, obj.pos.y, obj.radius, 0, 2*Math.PI);
+		ctx.arc(pos.x, pos.y, radius, 0, 2*Math.PI);
 		ctx.fill();
 	},
 	line: function(ctx,pos1,pos2,width,color) {
@@ -165,16 +151,16 @@ var draw = {
 		ctx.strokeStyle = color;
 		ctx.stroke();
 	},
-	clear: function(ctx) {
+	clear: function(ctx, canvas) {
 		ctx.fillStyle = 'white';
-		ctx.fillRect(0,0,gameCanvas.width, gameCanvas.height);
+		ctx.fillRect(0,0,canvas.width, canvas.height);
 	},
 
-	text: function(pos,size,color) {
-		
+	text: function(ctx,pos,size,color) {
+		// TODO
 	},
 	menu: function() {
-
+		// TODO
 	}
 };
 
@@ -203,23 +189,34 @@ var loop = {
 	game: function() {
 		frame++;
 
-		draw.clear(g);
+		draw.clear(g, gameCanvas);
+
 		func.applyPlayerSpeed(player);
-		fixBulletPos(player);
-		draw.circle(g,player);
+		update.swapArround(player);
+		draw.circle(g,player.pos, player.radius, player.color);
+
 		for(var i=0, len = bullets.array.length; i < len; i++) {
 			var bullet = bullets.array[i];
-			bullet.xspeed += 0.001;
-			bullet.yspeed += 0.001;
+			bullet.xspeed += 0.0005;
+			bullet.yspeed += 0.0005;
+
 			if(bullet.isConnected) {
 				func.applyPlayerSpeed(bullet);
 			}
+
 			update.collisions(bullet, i);
-			draw.circle(g,bullet);
+			draw.circle(g,bullet.pos,bullet.radius,bullet.color);
 		}
 
 		if(frame % 30 === 0) {
-			generateBullet(randInt(0,gameCanvas.width));
+			var newBullet = new Bullet(
+				vec2(0,Math.random(0,gameCanvas.width)),
+				6 + randInt(0,4),
+				randColor(),
+				vec2(1 + (2 * Math.random()), 1 + (2 * Math.random())),
+				pattern.sideWays
+			);
+			bullets.array.push(newBullet);
 		}
 
 		requestAnimationFrame(loop.cur);
